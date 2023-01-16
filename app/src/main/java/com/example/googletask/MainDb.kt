d6f7a77f49e53e0c9c214030f7492c0461c14dd0
bunbunsnookie <xbaka.so.baka@gmail.com>
2023-01-16 19:56:33 +1000

    import viewBinding

--- app/src/main/java/com/example/googletask/MainDb.kt
+++ app/src/main/java/com/example/googletask/MainDb.kt
@@ -8,6 +8,8 @@ import androidx.room.RoomDatabase
 @Database (entities = [Task::class], version = 1)
 abstract class MainDb : RoomDatabase() {
 
+    abstract fun getDao(): Dao
+
     companion object{
         fun getDb(context: Context): MainDb{
             return Room.databaseBuilder(
