3213edce9633af531d0ba3941f4efe7a425a439e
bunbunsnookie <xbaka.so.baka@gmail.com>
2023-01-17 19:52:21 +1000

    migration db

--- app/src/main/java/com/example/googletask/MainDb.kt
+++ app/src/main/java/com/example/googletask/MainDb.kt
@@ -1,11 +1,22 @@
 package com.example.googletask
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database (entities = [Task::class], version = 1)
+@Database (
+    entities = [Task::class],
+    version = 1,
+    exportSchema = true,
+    autoMigrations = [
+       AutoMigration(
+           from = 1,
+            to = 2,
+        )
+    ]
+)
 abstract class MainDb : RoomDatabase() {
 
     abstract fun getDao(): Dao
