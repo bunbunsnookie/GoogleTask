7f1f25b87862d4af2fe291e5f0e5daa27efb39ee
bunbunsnookie <xbaka.so.baka@gmail.com>
2023-01-18 19:06:47 +1000

    viewModel

---
+++ app/src/main/java/com/example/googletask/ViewModels/MainActivityViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.googletask.ViewModels
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.googletask.Models.TasksRepository
+
+@Suppress("UNCHECKED_CAST")
+class MainActivityViewModelFactory (application: Application ,private val repository: TasksRepository): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainActivityViewModel(repository) as T
+    }
+}
--- app/src/main/java/com/example/googletask/Views/MainActivity.kt
+++ app/src/main/java/com/example/googletask/Views/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.googletask.Views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.googletask.Models.MainDb
+import com.example.googletask.Models.TasksRepository
+import com.example.googletask.ViewModels.MainActivityViewModel
+import com.example.googletask.ViewModels.MainActivityViewModelFactory
 import com.example.googletask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), AddTaskDialog.CreateTaskDialogInterface {
@@ -11,6 +16,11 @@ class MainActivity : AppCompatActivity(), AddTaskDialog.CreateTaskDialogInterfac
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val database = MainDb.getInstance(this)
+        val repository = TasksRepository(database)
+        val factory = MainActivityViewModelFactory(application, repository)
+        val viewModel = ViewModelProvider(this, factory)[MainActivityViewModel::class.java]
 
         binding.apply {
             fab.setOnClickListener{
