e8185d1dde920c6fb83ae66bb941bac0abb01a6f
bunbunsnookie <xbaka.so.baka@gmail.com>
2023-01-19 01:16:41 +1000

    add task for activity

--- app/src/main/java/com/example/googletask/Views/MainActivity.kt
+++ app/src/main/java/com/example/googletask/Views/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.googletask.Views
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.googletask.Models.MainDb
-import com.example.googletask.Models.Task
-import com.example.googletask.Models.TasksRepository
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.googletask.Models.*
 import com.example.googletask.ViewModels.MainActivityViewModel
 import com.example.googletask.ViewModels.MainActivityViewModelFactory
 import com.example.googletask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), AddTaskDialog.CreateTaskDialogInterface {
     lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: TasksAdapter
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     private lateinit var viewModel: MainActivityViewModel
 
@@ -25,10 +31,30 @@ class MainActivity : AppCompatActivity(), AddTaskDialog.CreateTaskDialogInterfac
         val factory = MainActivityViewModelFactory(application, repository)
         viewModel = ViewModelProvider(this, factory)[MainActivityViewModel::class.java]
 
+        adapter =
+            TasksAdapter(viewModel.getAllTask().value?.reversed() ?: listOf(), viewModel) {
+                openEditTaskActivity(it)
+            }
+        layoutManager = LinearLayoutManager(this)
+        val tasksObserver = Observer<List<Task>> {
+            adapter.tasks = it.reversed()
+            adapter.notifyDataSetChanged()
+        }
+        viewModel.getAllTask().observe(this, tasksObserver)
+
         binding.apply {
             fab.setOnClickListener{
                 AddTaskDialog().show(supportFragmentManager, "Add task")
             }
+            edRecyclerTasks.adapter = adapter
+            edRecyclerTasks.layoutManager = layoutManager
+        }
+    }
+
+    private fun openEditTaskActivity(task: Task) {
+        Intent(this, TaskActivity::class.java).also { intent ->
+            intent.putExtra(Const.TASK_EXTRA, TaskSerializer.fromTask(task))
+            resultLauncher.launch(intent)
         }
     }
 
